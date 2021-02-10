b9b31a96ca5bdebe0d9b449b4dfe285e386e5681
Marc Jakobi <marc.jakobi@velasolaris.com>
2021-02-10 23:42:29 +0100

    Refactoring: Extract Layout.

---
+++ lib/Layout.hs
@@ -0,0 +1,30 @@
+module Layout where
+
+import XMonad
+import XMonad.Hooks.ManageDocks
+
+------------------------------------------------------------------------
+-- Layouts:
+
+-- You can specify and transform your layouts by modifying these values.
+-- If you change layout bindings be sure to use 'mod-shift-space' after
+-- restarting (with 'mod-q') to reset your layout state to the new
+-- defaults, as xmonad preserves your old layout settings by default.
+--
+-- The available layouts.  Note that each layout is separated by |||,
+-- which denotes layout choice.
+--
+myLayout = avoidStruts(tiled ||| Mirror tiled ||| Full)
+  where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+
+     -- The default number of windows in the master pane
+     nmaster = 1
+
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
+
--- xmonad.hs
+++ xmonad.hs
@@ -14,9 +14,9 @@ import XMonad.Util.SpawnOnce
 import Control.Monad
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.NoBorders
-import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Layout.Spacing
+import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Gaps
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
@@ -25,6 +25,7 @@ import Data.Maybe
 import qualified WindowState as WS
 import KeyBindings
 import MouseBindings
+import Layout
 
 -- The preferred terminal program, which is used in a binding below and by
 myTerminal :: String
@@ -85,32 +86,6 @@ addEWMHFullscreen   = do
     mapM_ addNETSupported [wms, wfs]
 
 
-
-
-------------------------------------------------------------------------
--- Layouts:
-
--- You can specify and transform your layouts by modifying these values.
--- If you change layout bindings be sure to use 'mod-shift-space' after
--- restarting (with 'mod-q') to reset your layout state to the new
--- defaults, as xmonad preserves your old layout settings by default.
---
--- The available layouts.  Note that each layout is separated by |||,
--- which denotes layout choice.
---
-myLayout = avoidStruts(tiled ||| Mirror tiled ||| Full)
-  where
-     -- default tiling algorithm partitions the screen into two panes
-     tiled   = Tall nmaster delta ratio
-
-     -- The default number of windows in the master pane
-     nmaster = 1
-
-     -- Default proportion of screen occupied by master pane
-     ratio   = 1/2
-
-     -- Percent of screen to increment by when resizing panes
-     delta   = 3/100
 
 ------------------------------------------------------------------------
 -- Window rules:
