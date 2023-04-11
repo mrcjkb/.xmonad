8b1dc59566df51a687252d3c10861a55e3f13467
Marc Jakobi <marc.jakobi@tiko.energy>
2023-04-11 22:44:54 +0200

    add xmobar flags

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -5,7 +5,33 @@ with final.lib; let
     overrides = final.lib.composeExtensions (old.overrides or (_: _: {})) (
       self: super: let
         xmonadrc = buildFromSdist (self.callPackage ../xmonadrc/default.nix {});
-        xmobar-app = buildFromSdist (self.callPackage ../xmobar-app/default.nix {});
+        xmobar-app = buildFromSdist (
+          overrideCabal (self.callPackage ../xmobar-app/default.nix {})
+          (old: {
+            configureFlags =
+              (old.configureFlags or [])
+              ++ [
+                "--ghc-options=-O2"
+                "--ghc-options=-Wall"
+                # Xmobar flags
+                "-fwith_alsa"
+                "-fwith_conduit"
+                "-fwith_datezone"
+                "-fwith_dbus"
+                "-fwith_inotify"
+                "-fwith_iwlib"
+                "-fwith_mpd"
+                "-fwith_mpris"
+                "-fwith_rtsopts"
+                "-fwith_threaded"
+                "-fwith_utf8"
+                "-fwith_uvmeter"
+                "-fwith_weather"
+                "-fwith_xft"
+                "-fwith_xpm"
+              ];
+          })
+        );
       in {
         inherit
           xmonadrc
