2e03f7e6f9482e80e0a40ec4f81ab8826a513dff
Marc Jakobi <marc.jakobi@tiko.energy>
2023-05-08 23:42:46 +0200

    formatting

--- xmobar-app/lib/Xmobar/Plugin/VolumeBar.hs
+++ xmobar-app/lib/Xmobar/Plugin/VolumeBar.hs
@@ -11,23 +11,29 @@ data VolumeBar = VolumeBar
 instance Exec VolumeBar where
   rate VolumeBar = 5
   run VolumeBar = do
-    vol <- readProcess "pamixer" ["--get-volume"] []
-    return (slider (parse vol) 100 10 setVolume "#" "·")
+    volume <- parse <$> readProcess "pamixer" ["--get-volume"] []
+    pure $ slider volume 10 setVolumeCmd "#" "·"
     where
       parse v = read (takeWhile (/= '%') v) :: Int
-      setVolume v = "pamixer --set-volume " ++ show (10 * v)
+      setVolumeCmd v = "pamixer --set-volume " ++ show (10 * v)
 
-slider :: Int -> Int -> Int -> (Int -> String) -> String -> String -> String
-slider value total width_ fun filledSymbol emptySymbol = wrap "<" ">" bar
+slider :: Int -> Int -> (Int -> String) -> String -> String -> String
+slider value barWidth mkSetVolumeCmd filledSymbol emptySymbol = wrap "<" ">" bar
   where
-    percentage :: Double = fromIntegral value / fromIntegral total
-    filledSize = ceiling (fromIntegral width_ * percentage)
-    -- bar = replicate filledSize 'X' ++ replicate (width - filledSize) '-'
-    symbol i = if i < filledSize then filledSymbol else emptySymbol
-    bar = concat [action (fun i) (symbol i) | i <- [0 .. width_ - 1]]
+    percentage :: Double = fromIntegral value / 100
+
+    filledSize = ceiling (fromIntegral barWidth * percentage)
+
+    mkSymbol :: Int -> String
+    mkSymbol i =
+      if i < filledSize
+        then filledSymbol
+        else emptySymbol
+
+    bar = concat [action (mkSetVolumeCmd i) (mkSymbol i) | i <- [0 .. barWidth - 1]]
 
     wrap :: String -> String -> String -> String
     wrap a1 a2 b = a1 <> b <> a2
 
     action :: String -> String -> String
-    action command = wrap ("<action=" ++ command ++ ">") "</action>"
+    action command = wrap ("<action=" <> command <> ">") "</action>"
