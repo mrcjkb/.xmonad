3f60c9bd60fedf1e23df78c986ce4c87be74d114
Marc Jakobi <marc.jakobi@tiko.energy>
2024-06-03 14:09:06 +0200

    update nixpkgs input

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
