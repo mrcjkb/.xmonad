0ddff747b4c9e7442d2a96a6a42ffa61a41b4608
Marc Jakobi <marc@jakobi.dev>
2026-01-16 09:54:44 +0100

    remove brave and firefox

--- nix/xmonad-session/default.nix
+++ nix/xmonad-session/default.nix
@@ -78,8 +78,6 @@
         pamixer # PulseAudio volume mixer
         pango # Rendering library used by xmobar
         bat
-        brave
-        firefox
       ])
       ++ (with pkgs.nur; [
         # nextcloud-client wrapper that waits for KeePass Secret Service Integration
